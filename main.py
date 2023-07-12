bc00c5fc6186d8198c0dc94a917626b8f136d0cd
devonLikesJava <devhulse04@icloud.com>
2023-07-12 09:05:26 -0400

    init commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import random
+
+
+guessBoard = [["x"] * 5 for i in range(5)]
+
+
+def printBoard():
+    print(guessBoard)
+
+def randomShips():
+    randomRow = random.randrange(1,5)
+    randomCol = random.randrange(1,5)
+    
+    # guessBoard[[0][]] = 
+
+def shipHit():
+    pass
+
+def shipMissed():
+    pass
+
+def shipSunk():
+    pass
+
+guessBoard[[0][1]] = 12
+
+printBoard()
